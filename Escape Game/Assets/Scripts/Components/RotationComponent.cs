8375421f050f254dc91d7bdddf0194e3214a1c9f
Lorenzo <neppy@live.nl>
2019-08-31 15:54:32 +0200

    Setup project

---
+++ Escape Game/Assets/Scripts/Components/RotationComponent.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Entities;
+using UnityEngine;
+
+[Serializable]
+public class RotationComponent : MonoBehaviour, IComponentData
+{
+    public float radiansPerSecond;
+}
---
+++ Escape Game/Assets/Scripts/Systems/RotationSystem.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Entities;
+
+public class RotationSystem : ComponentSystem
+{
+    protected override void OnUpdate()
+    {
+        // Entities.ForEach processes each set of ComponentData on the main thread. This is not the recommended
+        // method for best performance. However, we start with it here to demonstrate the clearer separation
+        // between ComponentSystem Update (logic) and ComponentData (data).
+        // There is no update logic on the individual ComponentData.
+        Entities.ForEach((ref RotationComponent rotationSpeed, ref Transform rotation) =>
+        {
+            var deltaTime = Time.deltaTime;
+            rotation.Value = math.mul(math.normalize(rotation.Value),
+                quaternion.AxisAngle(math.up(), rotationSpeed.RadiansPerSecond * deltaTime));
+        });
+    }
+}
