adcc93ddc460743ee77f3c4018ed28fd948955c5
Lorenzo <lorenzo.suiker@dutchworkz.nl>
2019-09-13 09:28:59 +0200

    Added beginning of healthSystem Lesson 2

---
+++ Escape Game/Assets/Tests/HealthSystemTests.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Assets.Scripts.Game;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class HealthSystemTests
+    {
+        [Test]
+        public void Can_Create()
+        {
+            HealthSystem hs = new GameObject().AddComponent<HealthSystem>();
+            Assert.IsNotNull(hs);
+        }
+    }
+}
