03139bf03907c26103794cb269fd7e746de2b11a
Lorenzo <lorenzo.suiker@dutchworkz.nl>
2019-09-20 08:49:09 +0200

    Updated tests and healthsystem

--- Escape Game/Assets/Scripts/Game/HealthSystem.cs
+++ Escape Game/Assets/Scripts/Game/HealthSystem.cs
@@ -5,5 +5,47 @@ namespace Assets.Scripts.Game
     public class HealthSystem : MonoBehaviour
     {
 
+        [Header("DEBUG INFO")]
+        [SerializeField]
+        private int _maxHealth;
+        public int MaxHealth
+        {
+            get => _maxHealth;
+            private set
+            {
+                _maxHealth = value;
+                if (CurrentHealth > _maxHealth)
+                    CurrentHealth = _maxHealth;
+            }
+        }
+
+        [SerializeField]
+        private int _currentHealth;
+        public int CurrentHealth
+        {
+            get => _currentHealth;
+            private set
+            {
+                _currentHealth = Mathf.Min(value, MaxHealth);
+            }
+        }
+
+        public static HealthSystem CreateHealthSystem(GameObject go, int maxHealth) => CreateHealthSystem(go, maxHealth, maxHealth);
+        public static HealthSystem CreateHealthSystem(GameObject go, int maxHealth, int currentHealth)
+        {
+            HealthSystem hs = go.AddComponent<HealthSystem>();
+            hs.MaxHealth = maxHealth;
+            hs.CurrentHealth = currentHealth;
+            return hs;
+        }
+
+        public void Damage(int damage)
+        {
+            CurrentHealth -= damage;
+        }
+        public void Heal(int heal)
+        {
+            CurrentHealth += heal;
+        }
     }
 }
--- Escape Game/Assets/Tests/HealthSystemTests.cs
+++ Escape Game/Assets/Tests/HealthSystemTests.cs
@@ -9,11 +9,41 @@ namespace Tests
 {
     public class HealthSystemTests
     {
+        private const int _DEFAULT_MAX_HP = 100;
+        private const int _DEFAULT_DMG_OR_HEAL = 20;
+
+        private static HealthSystem CreateHealthSystem(int maxHp, int currentHp) => HealthSystem.CreateHealthSystem(new GameObject(), maxHp, currentHp);
+        private static HealthSystem CreateHealthSystem() => CreateHealthSystem(_DEFAULT_MAX_HP, _DEFAULT_MAX_HP);
+
         [Test]
         public void Can_Create()
         {
-            HealthSystem hs = new GameObject().AddComponent<HealthSystem>();
+            HealthSystem hs = CreateHealthSystem();
             Assert.IsNotNull(hs);
+        }
+
+        [Test]
+        public void Healing_MoreThanZero_IncreasesHP()
+        {
+            HealthSystem hs = CreateHealthSystem(_DEFAULT_MAX_HP, _DEFAULT_DMG_OR_HEAL * 2);
+            hs.Heal(_DEFAULT_DMG_OR_HEAL);
+            Assert.AreEqual(_DEFAULT_DMG_OR_HEAL, hs.CurrentHealth);
+        }
+
+        [Test]
+        public void Healing_Zero_HPStaysSame()
+        {
+            HealthSystem hs = CreateHealthSystem(_DEFAULT_MAX_HP, _DEFAULT_DMG_OR_HEAL);
+            hs.Heal(0);
+            Assert.AreEqual(_DEFAULT_DMG_OR_HEAL, hs.CurrentHealth);
+        }
+
+        [Test]
+        public void Healing_DoesNotExceedMax()
+        {
+            HealthSystem hs = CreateHealthSystem();
+            hs.Heal(_DEFAULT_MAX_HP);
+            Assert.AreEqual(_DEFAULT_MAX_HP, hs.CurrentHealth);
         }
     }
 }
